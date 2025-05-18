89f70e259375c55fcf4adb0e22aadb860fe76244
Samjhana Neupane <samjhanaa.neupane@gmail.com>
2025-05-18 23:58:54 +0545

    footer

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Footer/footer.css'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='footerMain'>
+        <span class="footerTitle">Bhesbhusa</span>
+        <br></br>
+        <p>Copyright @2025 Bhesbhusa. All Rights Reserved.</p>
+    </div>
+    </>
+  )
+}
+
+
+export default Footer
